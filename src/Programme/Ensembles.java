d4c5c4543f4f9a62257d12e322900cbb18cc18fb
Mona Lisa <maxime.monceau@hotmail.fr>
2023-04-26 10:54:36 +0200

    gg

--- src/Programme/Ensembles.java
+++ src/Programme/Ensembles.java
@@ -6,40 +6,45 @@ public class Ensembles {
     protected ArrayList<Ensemble> List_Ensemble;
     protected double[][] MatriceDistance;
 
-    public Ensembles(double[][] MatriceDistance){
-        List_Ensemble=new ArrayList<>();
+    public Ensembles(double[][] MatriceDistance) {
+        List_Ensemble = new ArrayList<>();
         this.MatriceDistance = MatriceDistance;
     }
-    public void add_Ensemble(Ensemble ensemble){
+
+    public void add_Ensemble(Ensemble ensemble) {
         List_Ensemble.add(ensemble);
     }
-    public void del_Ensemble(int index){
+
+    public void del_Ensemble(int index) {
         List_Ensemble.remove(index);
     }
-    public double distanceMin(Ensemble e1, Ensemble e2){
+
+    public double distanceMin(Ensemble e1, Ensemble e2) {
         double distMin = Double.MAX_VALUE;
-        for(int i =0;i< e1.Images.size();i++){
-            for(int j=0;j<e2.Images.size();j++){
+        for (int i = 0; i < e1.Images.size(); i++) {
+            for (int j = 0; j < e2.Images.size(); j++) {
                 double dist = this.MatriceDistance[e1.Images.get(i)][e2.Images.get(j)];
-                if(dist < distMin){
-                    distMin=dist;
+                if (dist < distMin) {
+                    distMin = dist;
                 }
             }
         }
         return distMin;
     }
-    public double distanceMax(Ensemble e1, Ensemble e2){
+
+    public double distanceMax(Ensemble e1, Ensemble e2) {
         double distMax = Double.MIN_VALUE;
-        for(int i =0;i< e1.Images.size();i++){
-            for(int j=0;i<e2.Images.size();j++){
+        for (int i = 0; i < e1.Images.size(); i++) {
+            for (int j = 0; i < e2.Images.size(); j++) {
                 double dist = this.MatriceDistance[i][j];
-                if(dist > distMax){
-                    distMax=dist;
+                if (dist > distMax) {
+                    distMax = dist;
                 }
             }
         }
         return distMax;
     }
+
     public double distanceMoy(Ensemble e1, Ensemble e2) {
         double distMoy = 0;
         for (int i = 0; i < e1.Images.size(); i++) {
@@ -47,47 +52,47 @@ public class Ensembles {
                 distMoy += this.MatriceDistance[i][j];
             }
         }
-        return distMoy/(e1.Images.size()*e2.Images.size());
+        return distMoy / (e1.Images.size() * e2.Images.size());
     }
 
-    public void oneImgOneEns(){
+    public void oneImgOneEns() {
 
-        for(int i =0;i<MatriceDistance.length;i++){
+        for (int i = 0; i < MatriceDistance.length; i++) {
             Ensemble ensemblei = new Ensemble();
             ensemblei.add_Image(i);
             List_Ensemble.add(ensemblei);
         }
     }
 
-    public void fusionEnsemble(Ensemble e1, Ensemble e2,int index2){
-        for(int i =0;i<e2.Images.size();i++){
+    public void fusionEnsemble(Ensemble e1, Ensemble e2, int index2) {
+        for (int i = 0; i < e2.Images.size(); i++) {
             e1.add_Image(e2.Images.get(i));
         }
         this.del_Ensemble(index2);
     }
 
-    public ArrayList<Double> algoSautMin(){
+    public ArrayList<Double> algoSautMin() {
         this.oneImgOneEns();
         ArrayList<Double> distance = new ArrayList<>();
-        while(List_Ensemble.size()>1){
-            int ei =0;
-            int ej=0;
-            Ensemble ensemble1= null;
-            Ensemble ensemble2= null;
-            double plusproche=Double.MAX_VALUE;
-            for(int i =0;i<List_Ensemble.size();i++){
-                for(int j=i+1;j<List_Ensemble.size();j++){
-                    double distmin = this.distanceMin(List_Ensemble.get(i),List_Ensemble.get(j));
-                    if(distmin<plusproche){
-                        plusproche=distmin;
+        while (List_Ensemble.size() > 1) {
+            int ei = 0;
+            int ej = 0;
+            Ensemble ensemble1 = null;
+            Ensemble ensemble2 = null;
+            double plusproche = Double.MAX_VALUE;
+            for (int i = 0; i < List_Ensemble.size(); i++) {
+                for (int j = i + 1; j < List_Ensemble.size(); j++) {
+                    double distmin = this.distanceMin(List_Ensemble.get(i), List_Ensemble.get(j));
+                    if (distmin < plusproche) {
+                        plusproche = distmin;
                         ensemble1 = List_Ensemble.get(i);
                         ensemble2 = List_Ensemble.get(j);
-                        ei=i;
-                        ej=j;
+                        ei = i;
+                        ej = j;
                     }
                 }
             }
-            fusionEnsemble(ensemble1,ensemble2,ej);
+            fusionEnsemble(ensemble1, ensemble2, ej);
             distance.add(plusproche);
         }
         return distance;
@@ -97,56 +102,55 @@ public class Ensembles {
         return List_Ensemble;
     }
 
-    public void algoSautMin(Double[] heuristique){
+    public void algoSautMin(Double[] heuristique) {
         this.oneImgOneEns();
         ArrayList<Double> distance = new ArrayList<>();
         boolean drap = true;
-        while(drap){
-            int ei =0;
-            int ej=0;
-            Ensemble ensemble1= null;
-            Ensemble ensemble2= null;
-            double plusproche=Double.MAX_VALUE;
-            for(int i =0;i<List_Ensemble.size();i++){
-                for(int j=i+1;j<List_Ensemble.size();j++){
-                    double distmin = this.distanceMin(List_Ensemble.get(i),List_Ensemble.get(j));
-                    if(distmin<plusproche){
-                        plusproche=distmin;
+        while (drap) {
+            int ei = 0;
+            int ej = 0;
+            Ensemble ensemble1 = null;
+            Ensemble ensemble2 = null;
+            double plusproche = Double.MAX_VALUE;
+            for (int i = 0; i < List_Ensemble.size(); i++) {
+                for (int j = i + 1; j < List_Ensemble.size(); j++) {
+                    double distmin = this.distanceMin(List_Ensemble.get(i), List_Ensemble.get(j));
+                    if (distmin < plusproche) {
+                        plusproche = distmin;
                         ensemble1 = List_Ensemble.get(i);
                         ensemble2 = List_Ensemble.get(j);
-                        ei=i;
-                        ej=j;
+                        ei = i;
+                        ej = j;
                     }
                 }
             }
-            fusionEnsemble(ensemble1,ensemble2,ej);
+            fusionEnsemble(ensemble1, ensemble2, ej);
             distance.add(plusproche);
-            if(heuristique[0]==plusproche){
-                drap= false;
+            if (heuristique[0] == plusproche) {
+                drap = false;
             }
         }
     }
 
-
-
-    public Double[] calculerHeuristique(ArrayList<Double> distance){
-        double ecart=Double.MIN_VALUE;
-        double index= 0;
-        double index2=0;
-        for(int i=0;i<distance.size()-1;i++){
-            if(distance.get(i+1)-distance.get(i)>ecart){
-                ecart =distance.get(i+1)-distance.get(i);
-                index=distance.get(i);
-                index2=distance.get(i+1);
+    public Double[] calculerHeuristique(ArrayList<Double> distance) {
+        double ecart = Double.MIN_VALUE;
+        double index = 0;
+        double index2 = 0;
+        for (int i = 0; i < distance.size() - 1; i++) {
+            if (distance.get(i + 1) - distance.get(i) > ecart) {
+                ecart = distance.get(i + 1) - distance.get(i);
+                index = distance.get(i);
+                index2 = distance.get(i + 1);
             }
         }
 
         Double[] doubles = new Double[2];
-        doubles[0]= index;
-        doubles[1]= index2;
+        doubles[0] = index;
+        doubles[1] = index2;
         return doubles;
     }
-    public void resetEnsemble(){
+
+    public void resetEnsemble() {
         List_Ensemble.clear();
     }
 }
